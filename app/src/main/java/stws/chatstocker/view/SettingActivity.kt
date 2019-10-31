9eb5e6779414a96947184848bf1e04e8281516fe
avinash <avinash@stws.io>
2019-10-31 12:39:39 +0530

    31 10 12:54 avi

---
+++ app/src/main/java/stws/chatstocker/view/SettingActivity.kt
@@ -0,0 +1,13 @@
+package stws.chatstocker.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import stws.chatstocker.R
+
+class SettingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
